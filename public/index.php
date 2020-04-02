0540540da8718c584f826ddcdcd5359219b3a4eb
Alexandre Catherine-Mezeray <alexandre.catherine-mezeray@duproprio.com>
2020-04-02 17:31:09 -0400

    pagination

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ define('DEBUG_TIME', microtime(true));
 
 $router = new App\Router(dirname(__DIR__) . '/views');
 $router
-    ->get('/blog', '/post/index', 'blog')
+    ->get('/blog', '/post/index', 'home')
     ->get('/blog/[*:slug]-[i:id]', '/category/show', 'post')
     ->get('/blog/category', '/category/show', 'category')
     ->run();
